bfa80faf2a7915eaff623ad45c884d74875baf7d
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 23:38:32 +0700

    feat: update index.ts to include all page exports for better module accessibility

--- src/pages/list/index.ts
+++ src/pages/list/index.ts
@@ -1,5 +1,28 @@
+import AnnouncementPage from "./Announcement";
+import AssigmentPage from "./Assigment";
+import AttendancePage from "./Attendance";
+import ClassPage from "./Class";
+import EventPage from "./Event";
+import ExamPage from "./Exam";
 import HomePage from "./Home";
+import LessonPage from "./Lesson";
+import ParentPage from "./Parent";
+import ResultPage from "./Result";
 import StudentPage from "./Student";
+import SubjectPage from "./Subject";
 import TeacherPage from "./Teacher";
-
-export { HomePage, StudentPage, TeacherPage };
+export {
+  AnnouncementPage,
+  EventPage,
+  AttendancePage,
+  ResultPage,
+  AssigmentPage,
+  ExamPage,
+  LessonPage,
+  HomePage,
+  StudentPage,
+  TeacherPage,
+  ParentPage,
+  SubjectPage,
+  ClassPage,
+};
