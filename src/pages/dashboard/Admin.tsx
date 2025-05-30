ddd34920a1353e9e9d6360c2498ada4c8f7d2bf1
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 21:57:22 +0700

    feat: implement AdminPage layout with UserCard components

--- src/pages/dashboard/Admin.tsx
+++ src/pages/dashboard/Admin.tsx
@@ -1,7 +1,17 @@
+import { UserCard } from "../../components";
+
 const AdminPage = () => {
   return (
-    <div>
-      <p>AdminPage</p>
+    <div className="flex p-4 flex-col md:flex-row">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="Guru" />
+          <UserCard type="Siswa" />
+          <UserCard type="Orang Tua" />
+          <UserCard type="Staff" />
+        </div>
+        
+      </div>
     </div>
   );
 };
