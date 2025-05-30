8a07bc27ef792ae7c75896ecfc1e2e26c4709685
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 22:24:02 +0700

    feat: export StudentChart component from index for improved accessibility

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
+import StudentChart from "./charts/StudentChart";
 import Layout from "./layout";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import UserCard from "./UserCard";
-export { Navbar, Sidebar, Layout, UserCard };
+
+export { Navbar, Sidebar, Layout, UserCard, StudentChart };
