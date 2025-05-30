a75dccc35188d2a18dba4075447cbc22941da73b
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 21:41:07 +0700

    feat: add RoleDashboard to dashboard exports

--- src/pages/dashboard/index.ts
+++ src/pages/dashboard/index.ts
@@ -1,6 +1,7 @@
 import AdminPage from "./Admin";
 import ParentPage from "./Parent";
+import RoleDashboard from "./RoleDashboard";
 import StudentPage from "./Student";
 import TeacherPage from "./Teacher";
 
-export { AdminPage, ParentPage, StudentPage, TeacherPage };
+export { AdminPage, ParentPage, StudentPage, TeacherPage, RoleDashboard };
