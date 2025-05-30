fdad6ca7aa9b548bce0ed8e462c39569fc1243d2
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 23:38:50 +0700

    feat: simplify App component by replacing Routes with MainRoute for cleaner structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import { Layout } from "./components";
-import { RoleDashboard } from "./pages/dashboard";
-import { StudentPage, TeacherPage } from "./pages/list";
+import MainRoute from "./routes/route";
 import "./styles/App.css";
 
 function App() {
   // Path *'s
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<RoleDashboard />} />
-        <Route path="/teachers" element={<TeacherPage />} />
-        <Route path="/students" element={<StudentPage />} />
-      </Route>
-    </Routes>
-  );
+  return <MainRoute />;
 }
 
 export default App;
