402d89a477478ee445f15cfa052d824a8871cc2b
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 22:23:41 +0700

    style: update UserCard component styles for improved aesthetics and readability

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,14 +1,14 @@
 const UserCard = ({ type }: { type: string }) => {
   return (
-    <div className="rounded-2xl p-4 flex-1 min-w-[130px] bg-white border border-gray-300 ">
+    <div className="rounded-xl p-4 flex-1 min-w-[130px] odd:bg-success-500 even:bg-brand-500 shadow-lg">
       <div className="flex justify-between">
-        <span className="text-[10px] px-2 py-1 rounded-full text-gray-700">
+        <span className="text-[8px] self-center px-2 py-1 rounded-xl font-semibold bg-white">
           2025/26
         </span>
-        <i className="ri-more-2-fill text-lg cursor-pointer text-gray-500"></i>
+        <i className="ri-more-2-fill text-lg cursor-pointer text-white"></i>
       </div>
-      <h1 className="text-2xl font-bold my-4">1,234</h1>
-      <h1 className="text-sm font-medium text-gray-500">{type}</h1>
+      <h1 className="text-2xl font-bold my-4 text-white">1,234</h1>
+      <h1 className="text-sm font-medium text-white">{type}</h1>
     </div>
   );
 };
