14b8b34691dc6709a2530c07eda032fd1f67eabc
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 20:52:24 +0700

    feat: create dashboard page component exports

---
+++ src/pages/dashboard/index.ts
@@ -0,0 +1,6 @@
+import AdminPage from "./Admin";
+import ParentPage from "./Parent";
+import StudentPage from "./Student";
+import TeacherPage from "./Teacher";
+
+export { AdminPage, ParentPage, StudentPage, TeacherPage };
