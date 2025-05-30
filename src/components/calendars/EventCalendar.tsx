50d124af3b0ea88742e9195ad4da1c3b7b85b5e2
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 23:19:27 +0700

    feat: add EventCalendar component for event management and visualization

---
+++ src/components/calendars/EventCalendar.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { events } from "../../lib/data";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const EventCalendar = () => {
+  const [value, onChange] = useState<Value>(new Date());
+  return (
+    <div className="bg-white p-4 w-full h-full rounded-xl shadow border border-gray-200">
+      <Calendar onChange={onChange} value={value} />
+      <div className="flex my-4 justify-between items-center">
+        <h1 className="text-lg font-semibold">Events</h1>
+        <i className="ri-more-2-fill text-xl cursor-pointer"></i>
+      </div>
+      <div className="flex flex-col gap-4">
+        {events.map((event) => (
+          <div
+            className="p-5 rounded-md border-2 border-t-4 odd:border-brand-300 even:border-success-300"
+            key={event.id}
+          >
+            <div className="flex justify-between items-center">
+              <h1 className="text-gray-600 font-semibold">{event.title}</h1>
+              <span className="font-medium text-xs text-gray-400">
+                {event.time}
+              </span>
+            </div>
+            <p className="text-gray-400 text-xs font-medium mt-2">
+              {event.desriction}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventCalendar;
