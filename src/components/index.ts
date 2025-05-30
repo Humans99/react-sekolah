30f19424a17c4edeca2ef6739e964dd5d092790b
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 22:46:31 +0700

    feat: add CashFlowChart to index export for improved component accessibility

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,17 @@
 import AttendanceChart from "./charts/AttendanceChart";
+import CashFlowChart from "./charts/CashFlowChart";
 import StudentChart from "./charts/StudentChart";
 import Layout from "./layout";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import UserCard from "./UserCard";
 
-export { Navbar, Sidebar, Layout, UserCard, StudentChart, AttendanceChart };
+export {
+  Navbar,
+  Sidebar,
+  Layout,
+  UserCard,
+  StudentChart,
+  AttendanceChart,
+  CashFlowChart,
+};
