502e186107b1a8dd4441c2f325452b8fbf42fdc1
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 22:35:01 +0700

    feat: export AttendanceChart component from index for improved accessibility

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
+import AttendanceChart from "./charts/AttendanceChart";
 import StudentChart from "./charts/StudentChart";
 import Layout from "./layout";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import UserCard from "./UserCard";
 
-export { Navbar, Sidebar, Layout, UserCard, StudentChart };
+export { Navbar, Sidebar, Layout, UserCard, StudentChart, AttendanceChart };
