a9f66fe53318e48a5fdc9af532eaa2bae0a81534
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 21:33:46 +0700

    feat: add user card component

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,10 @@
+
+const UserCard = () => {
+  return (
+    <div className="rounded-2xl p-4 flex-1 min-w-[130px] bg-white border border-gray-300 ">
+      
+    </div>
+  )
+}
+
+export default UserCard
