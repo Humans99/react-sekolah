98c670b2e4741889f9ae3ed31435e54ce83f0bd0
Humans99 <samuelsidabalok12@gmail.com>
2025-05-30 23:21:06 +0700

    feat: reorganize imports in index.ts for improved readability

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,17 @@
+import EventCalendar from "./calendars/EventCalendar";
+
 import AttendanceChart from "./charts/AttendanceChart";
 import CashFlowChart from "./charts/CashFlowChart";
 import StudentChart from "./charts/StudentChart";
+
+import Announcement from "./Announcement";
 import Layout from "./layout";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import UserCard from "./UserCard";
-
 export {
+  Announcement,
+  EventCalendar,
   Navbar,
   Sidebar,
   Layout,
