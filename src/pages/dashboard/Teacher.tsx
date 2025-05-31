0651b38082ba12a10c3835680424b45d6a7eccaa
Humans99 <samuelsidabalok12@gmail.com>
2025-05-31 20:29:35 +0700

    feat: restructure TeacherPage component for improved layout and functionality

--- src/pages/dashboard/Teacher.tsx
+++ src/pages/dashboard/Teacher.tsx
@@ -1,7 +1,16 @@
+import { Announcement } from "../../components";
+
 const TeacherPage = () => {
   return (
-    <div>
-      <p>TeacherPage</p>
+    <div className="flex-1 flex gap-4 p-4 flex-col lg:flex-row">
+      <div className="w-full xl:w-2/3 flex-col flex">
+        <div className="bg-white p-4 w-full h-full rounded-xl shadow border border-gray-200">
+          <h1 className="text-xl font-semibold">Jadwal</h1>
+        </div>
+      </div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcement />
+      </div>
     </div>
   );
 };
