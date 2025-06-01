5acd93225754a825bcc77741cfe941665a21e071
Humans99 <samuelsidabalok12@gmail.com>
2025-06-01 22:35:16 +0700

    feat: add teacherColumns configuration for teacher data display

---
+++ src/lib/list.ts
@@ -0,0 +1,35 @@
+export const teacherColumns = [
+  {
+    header: "Info",
+    key: "info",
+  },
+  {
+    header: "Kode Guru",
+    key: "teacherId",
+    className: "hidden md:table-cell whitespace-nowrap",
+  },
+  {
+    header: "Mata Pelajaran",
+    key: "subjects",
+    className: "hidden md:table-cell whitespace-nowrap",
+  },
+  {
+    header: "Kelas",
+    key: "classes",
+    className: "hidden md:table-cell whitespace-nowrap",
+  },
+  {
+    header: "Telepon",
+    key: "phone",
+    className: "hidden lg:table-cell whitespace-nowrap",
+  },
+  {
+    header: "Alamat",
+    key: "address",
+    className: "hidden lg:table-cell whitespace-nowrap",
+  },
+  {
+    header: "Aksi",
+    key: "action",
+  },
+];
