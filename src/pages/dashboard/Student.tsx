b5b3a67cc0fe1c6b247fe364780af7db403d673f
Humans99 <samuelsidabalok12@gmail.com>
2025-06-01 21:48:00 +0700

    feat: restructure StudentPage layout to include BigCalendar, EventCalendar, and Announcement components

--- src/pages/dashboard/Student.tsx
+++ src/pages/dashboard/Student.tsx
@@ -1,7 +1,21 @@
+import { Announcement, BigCalendar, EventCalendar } from "../../components";
+
 const StudentPage = () => {
   return (
-    <div>
-      <p>StudentPage</p>
+    <div className="flex p-4 gap-4 flex-col xl:flex-row">
+      <div className="w-full xl:w-2/3 flex-col flex">
+        <div className="bg-white p-4 h-full rounded-xl shadow border border-gray-200 overflow-hidden">
+          <h1 className="text-xl font-semibold">Jadwal</h1>
+          <div className="w-full h-full">
+            <BigCalendar />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcement />
+      </div>
     </div>
   );
 };
