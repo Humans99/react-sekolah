6c27e040ed0afde632ccf42110dd74c5c205b70b
Humans99 <samuelsidabalok12@gmail.com>
2025-06-01 21:58:11 +0700

    feat: implement layout for ParentPage with BigCalendar and Announcement components

--- src/pages/dashboard/Parent.tsx
+++ src/pages/dashboard/Parent.tsx
@@ -1,7 +1,20 @@
+import { Announcement, BigCalendar } from "../../components";
+
 const ParentPage = () => {
   return (
-    <div>
-      <p>ParentPage</p>
+    <div className="flex p-4 gap-4 flex-col xl:flex-row">
+      <div className="w-full xl:w-2/3 flex-col flex">
+        <div className="bg-white p-4 w-full h-screen rounded-xl shadow border border-gray-200 overflow-hidden">
+          <h1 className="text-xl font-semibold">Jadwal (John Doe)</h1>
+          <div className="h-full w-full">
+            <BigCalendar />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 h-screen">
+        <Announcement />
+      </div>
     </div>
   );
 };
