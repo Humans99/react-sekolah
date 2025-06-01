cfb04a75ebc63ae3dd18464a7354fcb25b0a141b
Humans99 <samuelsidabalok12@gmail.com>
2025-06-01 22:35:01 +0700

    feat: add TableSearch component for search functionality in tables

---
+++ src/components/table/TableSearch.tsx
@@ -0,0 +1,17 @@
+const TableSearch = () => {
+  return (
+    <div className="w-full md:w-auto flex  items-center gap-2 text-xs rounded-lg ring-[1px] ring-gray-300 px-2">
+      <i className="ri-search-line text-lg text-gray-500"></i>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        autoComplete="off"
+        placeholder="Cari ..."
+        className="w-52 p-3 bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+export default TableSearch;
