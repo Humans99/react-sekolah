abefab88b0d93e6a0d27e0de69f043670057e815
Humans99 <samuelsidabalok12@gmail.com>
2025-06-01 21:28:17 +0700

    feat: enhance TeacherPage layout and integrate BigCalendar component

--- src/pages/dashboard/Teacher.tsx
+++ src/pages/dashboard/Teacher.tsx
@@ -1,14 +1,19 @@
-import { Announcement } from "../../components";
+import { Announcement, BigCalendar } from "../../components";
 
 const TeacherPage = () => {
   return (
-    <div className="flex-1 flex gap-4 p-4 flex-col lg:flex-row">
-      <div className="w-full xl:w-2/3 flex-col flex">
-        <div className="bg-white p-4 w-full h-full rounded-xl shadow border border-gray-200">
+    <div className="flex-1 flex gap-4 p-4 flex-col xl:flex-row">
+      <div className="w-full xl:w-2/3 flex-col flex min-h-[400px]">
+        {/* <div className="flex flex-col flex-1 min-h-[400px]:"> */}
+        <div className="bg-white p-4 w-full h-[90vh] rounded-xl shadow border border-gray-200 overflow-hidden">
           <h1 className="text-xl font-semibold">Jadwal</h1>
+          <div className="h-full w-full">
+            <BigCalendar />
+          </div>
+          {/* <DefaultCalendar /> */}
         </div>
       </div>
-      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 h-[90vh]">
         <Announcement />
       </div>
     </div>
