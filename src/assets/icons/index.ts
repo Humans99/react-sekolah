d861a1c5d3ba1f51e6ce1acbe4fc04a42174f10c
Humans99 <samuelsidabalok12@gmail.com>
2025-06-02 21:56:44 +0700

    feat: create index for icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,20 @@
+import singleAtendance from "./singleAttendance.png";
+import singleBranch from "./singleBranch.png";
+import singleClass from "./singleClass.png";
+import singleLesson from "./singleLesson.png";
+
+import blood from "./blood.png";
+import date from "./date.png";
+import mail from "./mail.png";
+import phone from "./phone.png";
+
+export {
+  singleAtendance,
+  singleBranch,
+  singleClass,
+  singleLesson,
+  blood,
+  date,
+  mail,
+  phone,
+};
