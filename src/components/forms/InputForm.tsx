9d6971787f36bc7bc4531d54cdc8ee15dd1244be
Humans99 <samuelsidabalok12@gmail.com>
2025-06-03 20:43:33 +0700

    feat: add InputForm component for user input handling

---
+++ src/components/forms/InputForm.tsx
@@ -0,0 +1,50 @@
+import type React from "react";
+import type { FieldError } from "react-hook-form";
+
+type Props = {
+  label: string;
+  type: string;
+  register: any;
+  name: string;
+  defaultValue?: string;
+  error?: FieldError;
+  inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
+};
+
+const InputForm = ({
+  label,
+  type = "text",
+  register,
+  name,
+  defaultValue,
+  error,
+  inputProps,
+}: Props) => {
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <div className="text-gray-500 text-xs font-medium after:content-['*'] after:text-error-500 after:ml-1">
+        {label}
+      </div>
+      <input
+        className={`text-gray-600 font-medium ring-[1.5px] p-2 rounded-md text-sm outline-none focus:ring-brand-500 focus:pl-3 focus:ring-[1.5px] transition-all duration-300 w-full ${
+          error ? "ring-error-300" : "ring-gray-300"
+        }`}
+        type={type ?? "text"}
+        name={name}
+        id={name}
+        placeholder={`${label} ......`}
+        autoComplete="off"
+        {...register(name)}
+        defaultValue={defaultValue}
+        {...inputProps}
+      />
+      {error?.message && (
+        <p className="text-xs text-error-500 font-medium">
+          {error.message.toString()}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default InputForm;
