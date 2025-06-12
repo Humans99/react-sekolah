cebc8eeb89f0a1244a30ae6dac4135edac130ff0
Humans99 <samuelsidabalok12@gmail.com>
2025-06-12 20:02:20 +0700

    feat: add SignIn and SignUp components with form validation

---
+++ src/pages/auth/SignIn.tsx
@@ -0,0 +1,69 @@
+import { z } from "zod";
+import { InputForm } from "../../components";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
+
+const schema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+const SignIn = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log("Form data submitted:", data);
+    navigate("/dashboard");
+  });
+  return (
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <div className=" p-6 bg-white rounded-lg shadow-md w-[440px]">
+        <h1 className="text-center font-medium text-md lg:text-lg">Sign In</h1>
+        <form action="" onSubmit={onSubmit}>
+          <div className="flex flex-col gap-4 mt-4">
+            <InputForm
+              label="Email"
+              name="email"
+              type="email"
+              register={register}
+              defaultValue=""
+              error={errors.email}
+            />
+            <InputForm
+              label="Password"
+              name="password"
+              type="password"
+              register={register}
+              defaultValue=""
+              error={errors.password}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-brand-500 hover:bg-brand-600 text-white font-semibold px-4 py-2 rounded-md mt-6 transition-colors duration-300 cursor-pointer"
+          >
+            Sign In
+          </button>
+          <p className="text-center text-sm text-gray-500 mt-2">
+            Don't have an account?{" "}
+            <a href="/register" className="text-brand-500 hover:underline">
+              Sign Up
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
