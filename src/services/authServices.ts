41258d815380cdc081973d29cfbef13df9225023
Humans99 <samuelsidabalok12@gmail.com>
2025-06-16 22:01:04 +0700

    feat: store user role and username in localStorage upon login

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -3,6 +3,8 @@ import { login, register } from "../api";
 export const loginUser = async (data: any) => {
   const response = await login(data);
   localStorage.setItem("user_token", response.data.token);
+  localStorage.setItem("role", response.data.user.role);
+  localStorage.setItem("username", response.data.user.username);
   return response.data;
 };
 
