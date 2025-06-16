18be7c18f7c8d0a205147831263878302c811796
Humans99 <samuelsidabalok12@gmail.com>
2025-06-16 22:01:31 +0700

    refactor: clean up axios interceptor code formatting

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,15 +4,21 @@ const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api",
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("user_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("user_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config
-});
+);
 
-export default api
+export default api;
