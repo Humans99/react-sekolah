480fd90a3a5d02a6e23201ab4bcd34666fe94c49
Humans99 <samuelsidabalok12@gmail.com>
2025-06-18 12:14:22 +0700

    feat: add deleteTeacher service to manage teacher deletions

--- src/services/teacherServices.ts
+++ src/services/teacherServices.ts
@@ -1,11 +1,21 @@
 import api from "../api/axios";
 
-export const getAllTeachers = async (url: string = '/teachers') => {
+export const getAllTeachers = async (url: string = "/teachers") => {
   try {
     const response = await api.get(url);
     return response.data;
   } catch (error) {
     console.log("Error fetching teachers data: ", error);
+    throw error;
+  }
+};
+
+export const deleteTeacher = async (code: string) => {
+  try {
+    const response = await api.delete(`/teachers/code/${code}`);
+    return response.data;
+  } catch (error) {
+    console.log("Error delete teachers data", error);
     throw error;
   }
 };
