ec7a02104d412f998f0d6c974ec5ae63eaca999a
Humans99 <samuelsidabalok12@gmail.com>
2025-06-18 12:13:54 +0700

    feat: include deleteTeacher service in index export

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,3 @@
 import { loginUser, registerUser } from "./authServices";
-import { getAllTeachers } from "./teacherServices";
-export { loginUser, registerUser, getAllTeachers };
+import { getAllTeachers, deleteTeacher } from "./teacherServices";
+export { loginUser, registerUser, getAllTeachers, deleteTeacher };
