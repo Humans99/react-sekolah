ffca922f40d4808a406a372d58b8523036347768
Humans99 <samuelsidabalok12@gmail.com>
2025-06-18 12:15:39 +0700

    feat: enhance TeacherRow component to support delete functionality

--- src/components/table/TeacherRow.tsx
+++ src/components/table/TeacherRow.tsx
@@ -14,21 +14,19 @@ type Props = {
     user: { email: string };
   };
   role: string | null;
+  onSuccessDelete: () => void;
 };
 
-const TeacherRow = ({ item, role }: Props) => {
+const TeacherRow = ({ item, role, onSuccessDelete }: Props) => {
   const navigate = useNavigate();
 
   return (
-    <tr
-      className="border-b border-gray-300 even:bg-slate-50 text-sm hover:bg-slate-200"
-      key={item.id}
-    >
+    <tr className="border-b border-gray-300 even:bg-slate-50 text-sm hover:bg-slate-200">
       <td className="flex gap-4 items-center p-4">
         <div className="h-10 w-10 bg-gray-400 rounded-full hidden md:table-cell"></div>
         <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <h3 className="text-sm text-gray-400">{item?.user?.email}</h3>
+          <h3 className="font-semibold whitespace-nowrap">{item.name}</h3>
+          <h3 className="text-sm text-gray-500">{item?.user?.email}</h3>
         </div>
       </td>
       <td className="whitespace-nowrap hidden md:table-cell">{item.code}</td>
@@ -41,12 +39,18 @@ const TeacherRow = ({ item, role }: Props) => {
         <div className="flex items-center gap-2">
           <button
             className="w-9 h-9 bg-warning-500 rounded-full shadow items-center justify-center cursor-pointer"
-            onClick={() => navigate(`/teachers/${item.code}`)}
+            onClick={() => navigate(`/teachers/code/${item.code}`)}
           >
             <i className="ri-eye-line text-lg text-slate-100"></i>
           </button>
           {role === "admin" && (
-            <FormModal table="teacher" type="delete" id={item.id} />
+            <FormModal
+              table="teacher"
+              type="delete"
+              data={item}
+              code={item.code}
+              onSuccessDelete={onSuccessDelete}
+            />
           )}
         </div>
       </td>
