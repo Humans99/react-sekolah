2ea0ea1c1ac24e136702efb5a9247b87de98c576
Humans99 <samuelsidabalok12@gmail.com>
2025-06-19 15:35:46 +0700

    fix: enhance submit button in TeacherForm with loading state and spinner

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -130,8 +130,41 @@ const TeacherForm = ({
           error={errors?.subject_id}
         />
       </div>
-      <button className="text-white mt-4 -mb-5 bg-brand-500 px-4 py-2 rounded-lg hover:bg-brand-600 cursor-pointer shadow-lg transition-all duration-300">
-        Submit
+      <button
+        disabled={loading}
+        className={`text-white mt-4 -mb-5 px-4 py-2 rounded-lg shadow-lg transition-all duration-300 ${
+          loading
+            ? "bg-gray-500 cursor-not-allowed "
+            : "bg-brand-500 hover:bg-brand-600 cursor-pointer"
+        }`}
+      >
+        {loading ? (
+          <div className="flex items-center gap-4 justify-center">
+            <svg
+              className="animate-spin h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+              ></path>
+            </svg>
+            Submiting ....
+          </div>
+        ) : (
+          "Submit"
+        )}
       </button>
     </form>
   );
