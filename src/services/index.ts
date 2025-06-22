ed21a542f9f0b23180290942cf93ceaa8ddc0cdd
Humans99 <samuelsidabalok12@gmail.com>
2025-06-22 22:04:31 +0700

    fix: add missing getTeacher export in services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,17 @@
+// Auth
 import { loginUser, registerUser } from "./authServices";
+
+// Teacher
 import {
   getAllTeachers,
   deleteTeacher,
   createTeacher,
+  getTeacher,
 } from "./teacherServices";
+
+// Export
 export {
+  getTeacher,
   loginUser,
   registerUser,
   getAllTeachers,
