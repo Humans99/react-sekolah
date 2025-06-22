4e944a112a949bdb1c8120d720cc14b2204d5793
Humans99 <samuelsidabalok12@gmail.com>
2025-06-22 22:03:46 +0700

    fix: add overflow handling to SmallCard component for better content display

--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -22,7 +22,7 @@ const SmallCard = ({ title, desc, type }: Props) => {
   return (
     <div className="flex gap-4 w-full bg-white border border-gray-200 shadow rounded-md p-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
       <img src={iconMap[type]} width={24} height={24} className="h-6 w-6" />
-      <div className="flex flex-col">
+      <div className="flex flex-col overflow-auto">
         <h1 className="text-xl font-semibold">{title}</h1>
         <span className="text-sm text-gray-500">{desc}</span>
       </div>
