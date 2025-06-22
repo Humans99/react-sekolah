fe59d7253940e1e7399bf317d1f4b9388acef485
Humans99 <samuelsidabalok12@gmail.com>
2025-06-22 22:04:22 +0700

    feat: add getTeacher service for fetching individual teacher data

--- src/services/teacherServices.ts
+++ src/services/teacherServices.ts
@@ -10,6 +10,16 @@ export const getAllTeachers = async (url: string = "/teachers") => {
   }
 };
 
+export const getTeacher = async (code: string) => {
+  try {
+    const response = await api.get(`/teachers/code/${code}`);
+    return response.data;
+  } catch (error) {
+    console.log("Error fetching teacher data: ", error);
+    throw error
+  }
+};
+
 export const deleteTeacher = async (code: string) => {
   try {
     const response = await api.delete(`/teachers/code/${code}`);
