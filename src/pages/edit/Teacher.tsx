ce568ee0e06aad7b939df2020286ffdbbd96c994
Humans99 <samuelsidabalok12@gmail.com>
2025-06-22 22:04:00 +0700

    feat: implement teacher data fetching and display in EditTeacher component

--- src/pages/edit/Teacher.tsx
+++ src/pages/edit/Teacher.tsx
@@ -1,27 +1,45 @@
+import { useCallback, useEffect, useState } from "react";
 import { blood, date, mail, phone } from "../../assets/icons";
 import {
   Announcement,
   BigCalendar,
-  FormModal,
+  // FormModal,
   PerformanceChart,
   Shortcut,
   SmallCard,
 } from "../../components";
+import { getTeacher } from "@/services";
+import { useParams } from "react-router-dom";
 
-const data = {
-  id: 1,
-  username: "johndoe",
-  email: "johndoe@example.com",
-  password: "password",
-  fullName: "john doe",
-  phone: "082130706225",
-  address: "123 Main St, Anytown USA",
-  bloodType: "A",
-  birthDay: "2001-11-23",
-  gender: "Pria",
+type Teacher = {
+  id: number;
+  code: string;
+  name: string;
+  phone: string;
+  address: string;
+  nip: string;
+  bloodType: string;
+  gender: string;
+  user: { username: string; email: string };
+  subject: { name: string };
 };
 
 const EditTeacher = () => {
+  const { teacherCode } = useParams();
+  const [teacher, setTeacher] = useState<Teacher | null>(null);
+  const fetchTeacher = useCallback(async ({ code }: { code: string }) => {
+    try {
+      const response = await getTeacher(code!);
+      setTeacher(response.data);
+    } catch (error) {
+      console.log("Gagal memuat data guru: ", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchTeacher({ code: teacherCode! });
+  }, []);
+
   return (
     <div className="flex flex-1 flex-col p-4 xl:flex-row gap-4">
       <div className="w-full xl:w-2/3">
@@ -30,38 +48,30 @@ const EditTeacher = () => {
           {/* User Card */}
           <div className="py-6 px-4 rounded-md bg-white shadow border border-gray-200 flex-1 flex gap-4">
             <div className="w-1/3">
-              <img
-                src="https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1200"
-                width={144}
-                height={144}
-                className="w-36 h-36 object-cover rounded-full"
-              />
+              <div className="w-36 h-36 bg-gray-400 rounded-full shadow-lg"></div>
             </div>
             <div className="w-2/3 flex-col flex justify-between gap-4">
               <div className="flex items-center justify-between">
-                <h1 className="text-xl font-semibold">John Doe</h1>
-                <FormModal table="teacher" type="update" data={data} />
+                <h1 className="text-xl font-semibold">{teacher?.name}</h1>
+                {/* <FormModal table="teacher" type="update" data={data} /> */}
               </div>
-              <p className="text-xs text-gray-400">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-                at?
-              </p>
+              <p className="text-xs text-gray-400">{teacher?.address}</p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
                   <img src={blood} alt="" width={14} height={14} />
-                  <span>B</span>
+                  <span>{teacher?.bloodType}</span>
                 </div>
                 <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
                   <img src={date} alt="" width={14} height={14} />
-                  <span>Nov, 2001</span>
+                  <span>{teacher?.gender}</span>
                 </div>
                 <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
                   <img src={mail} alt="" width={14} height={14} />
-                  <span>johndoe@gmail.com</span>
+                  <span>{teacher?.user?.email}</span>
                 </div>
                 <div className="flex items-center gap-2 w-full md:w-1/3 lg:w-full 2xl:w-1/3">
                   <img src={phone} alt="" width={14} height={14} />
-                  <span>082130706225</span>
+                  <span>{teacher?.phone}</span>
                 </div>
               </div>
             </div>
@@ -70,8 +80,12 @@ const EditTeacher = () => {
           {/* Small Card */}
           <div className="flex flex-1 gap-4 justify-between flex-wrap">
             <SmallCard type="attendance" desc="Kehadiran" title="90%" />
-            <SmallCard type="branch" desc="Cabang" title="2" />
-            <SmallCard type="lesson" desc="Pelajaran" title="6" />
+            <SmallCard type="branch" desc="NIP" title={teacher?.nip ?? ""} />
+            <SmallCard
+              type="lesson"
+              desc="Pelajaran"
+              title={teacher?.subject?.name ?? ""}
+            />
             <SmallCard type="class" desc="Kelas" title="10" />
           </div>
         </div>
