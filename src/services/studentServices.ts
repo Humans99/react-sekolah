74e11d99afd3815ce1c755c0c83ca354c452a158
Humans99 <samuelsidabalok12@gmail.com>
2025-06-26 14:14:15 +0700

    feat: add createStudent and deleteStudent functions to studentServices

--- src/services/studentServices.ts
+++ src/services/studentServices.ts
@@ -9,3 +9,23 @@ export const getAllStudents = async (url: string = "/students") => {
     throw error;
   }
 };
+
+export const createStudent = async (data: any) => {
+  try {
+    const response = await api.post("/students/student-register", data);
+    return response.data;
+  } catch (error) {
+    console.log("Error create students: ", error);
+    throw error;
+  }
+};
+
+export const deleteStudent = async (nis: string) => {
+  try {
+    const response = await api.delete(`/students/nis/${nis}`);
+    return response.data;
+  } catch (error) {
+    console.log("Error to delete student data: ", error);
+    throw error;
+  }
+};
