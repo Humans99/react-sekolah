eb5917d2ba3f67ffd997903b8a86de2d30bd8f90
Humans99 <samuelsidabalok12@gmail.com>
2025-06-26 14:13:52 +0700

    feat: refactor StudentForm to separate create and update schemas, add student creation logic with success handling

--- src/components/forms/StudentForm.tsx
+++ src/components/forms/StudentForm.tsx
@@ -4,20 +4,20 @@ import { z } from "zod";
 import InputForm from "./InputForm";
 import SelectForm from "./SelectForm";
 import { useState } from "react";
+import { createStudent } from "@/services";
+import SuccessToast from "../toast/Success";
+import { toast } from "sonner";
 
-const schema = z.object({
+// ----------- Schema -------------
+
+const createSchema = z.object({
   name: z.string().min(1, { message: "name is required" }),
-  nis: z.string().min(1),
   gender: z.enum(["Pria", "Wanita"], { message: "gender is required" }),
   phone: z
     .string()
     .min(11, { message: "phone number must be at least 11 number" }),
   bloodType: z.enum(["A", "B", "AB", "O"], {
     message: "blood type is required",
-  }),
-  grade_id: z.enum(["1", "2", "3"], { message: "grade is required" }),
-  class_id: z.enum(["A", "B", "C", "D", "E", "F", "G", "H", "I"], {
-    message: "class is required",
   }),
   email: z.string().email({ message: "invalid email address" }),
   // Parent
@@ -27,26 +27,71 @@ const schema = z.object({
     .string()
     .min(11, { message: "phone number must be at least 11 number" }),
   parent_address: z.string().min(1, { message: "address is required" }),
+  grade_id: z.enum(["1", "2", "3"], { message: "grade is required" }),
+  class_id: z.enum(["1", "2", "3", "4", "5", "6", "7", "8", "9"], {
+    message: "class is required",
+  }),
 });
 
-const StudentForm = ({
-  type,
-  data,
-  onSuccessCreate,
-}: {
+const updateSchema = z.object({
+  name: z.string().min(1, { message: "name is required" }),
+  gender: z.enum(["Pria", "Wanita"], { message: "gender is required" }),
+  phone: z
+    .string()
+    .min(11, { message: "phone number must be at least 11 number" }),
+  bloodType: z.enum(["A", "B", "AB", "O"], {
+    message: "blood type is required",
+  }),
+  email: z.string().email({ message: "invalid email address" }),
+  // Parent
+  parent_name: z.string().min(1, { message: "parent name is required" }),
+  parent_email: z.string().email({ message: "invalid email address" }),
+  parent_phone: z
+    .string()
+    .min(11, { message: "phone number must be at least 11 number" }),
+  parent_address: z.string().min(1, { message: "address is required" }),
+  grade_id: z.enum(["1", "2", "3"], { message: "grade is required" }),
+  class_id: z.enum(["1", "2", "3", "4", "5", "6", "7", "8", "9"], {
+    message: "class is required",
+  }),
+  nis: z.string().min(1, { message: "nis is required" }),
+});
+
+// --------- Props --------------
+
+type StudentFormProps = {
   type: "create" | "update";
   data?: any;
-  onSuccessDelete?: () => void;
-}) => {
+  onSuccessCreate?: () => void;
+};
+
+type CreateStudentForm = z.infer<typeof createSchema>;
+type UpdateStudentForm = z.infer<typeof updateSchema>;
+type StudentFormType = CreateStudentForm | UpdateStudentForm;
+
+const StudentForm = ({ type, data, onSuccessCreate }: StudentFormProps) => {
+  const schema = type === "create" ? createSchema : updateSchema;
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ resolver: zodResolver(schema) });
+  } = useForm<StudentFormType>({
+    resolver: zodResolver(schema),
+  });
   const [loading, setLoading] = useState(false);
 
   const onSubmit = handleSubmit(async (data) => {
-    console.log(data);
+    try {
+      setLoading(true);
+      const response = await createStudent(data);
+      SuccessToast({ message: response.message });
+      onSuccessCreate?.();
+    } catch (error) {
+      toast.error("Error to create student. ");
+      console.log("Error to create student. ", error);
+    } finally {
+      setLoading(false);
+    }
   });
 
   return (
@@ -66,32 +111,16 @@ const StudentForm = ({
           type="text"
         />
         {type === "update" && (
-          <>
-            <InputForm
-              label="NIS"
-              name="nis"
-              placeholder="1103204012"
-              defaultValue={data?.nis}
-              register={register}
-              error={errors?.nis}
-              type="number"
-              disabled
-            />
-            <SelectForm
-              label="Tingkat Kelas"
-              name="grade_id"
-              defaultValue={data?.grade_id}
-              register={register}
-              error={errors?.grade_id}
-            />
-            <SelectForm
-              label="Kelas"
-              name="class_id"
-              defaultValue={data?.class_id}
-              register={register}
-              error={errors?.class_id}
-            />
-          </>
+          <InputForm
+            label="NIS"
+            name="nis"
+            placeholder="1103204012"
+            defaultValue={data?.nis}
+            register={register}
+            error={(errors as any)?.nis}
+            type="number"
+            disabled
+          />
         )}
         <InputForm
           label="Email"
@@ -101,6 +130,20 @@ const StudentForm = ({
           register={register}
           error={errors?.email}
           type="text"
+        />
+        <SelectForm
+          label="Tingkat Kelas"
+          name="grade_id"
+          defaultValue={data?.grade_id}
+          register={register}
+          error={errors?.grade_id}
+        />
+        <SelectForm
+          label="Kelas"
+          name="class_id"
+          defaultValue={data?.class_id}
+          register={register}
+          error={errors?.class_id}
         />
         <InputForm
           label="Telepon"
